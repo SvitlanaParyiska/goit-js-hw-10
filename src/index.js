13bb28f14f3cc2c1482ec75cb96fa78e3a1c135e
SvitlanaParyiska <paryiskasvitlana@gmail.com>
2023-07-27 23:08:41 +0300

    n

--- src/index.js
+++ src/index.js
@@ -7,45 +7,45 @@ const boxCatInfoEl = document.querySelector('.cat-info');
 
 selectEl.addEventListener('change', setOutput);
 
-loaderEl.classList.remove('visually-hidden');
-fetchBreeds()
-  .then(data => {
-    selectEl.innerHTML = createSelectList(data);
-    loaderEl.classList.add('visually-hidden');
-  })
-  .catch(err => {
-    loaderEl.classList.add('visually-hidden');
-    errorEl.classList.remove('is-hidden');
-  });
+// loaderEl.classList.remove('visually-hidden');
+// fetchBreeds()
+//   .then(data => {
+//     selectEl.innerHTML = createSelectList(data);
+//     loaderEl.classList.add('visually-hidden');
+//   })
+//   .catch(err => {
+//     loaderEl.classList.add('visually-hidden');
+//     errorEl.classList.remove('is-hidden');
+//   });
 
-function createSelectList(arr) {
-  return arr
-    .map(({ name, id }) => `<option value=${id}>${name}</option>`)
-    .join('');
-}
+// function createSelectList(arr) {
+//   return arr
+//     .map(({ name, id }) => `<option value=${id}>${name}</option>`)
+//     .join('');
+// }
 
-function setOutput() {
-  boxCatInfoEl.innerHTML = '';
-  loaderEl.classList.remove('visually-hidden');
-  return fetchCatByBreed(selectEl.value)
-    .then(data => {
-      boxCatInfoEl.innerHTML = createCatInfo(data);
-      loaderEl.classList.add('visually-hidden');
-    })
-    .catch(err => {
-      errorEl.classList.remove('is-hidden');
-      loaderEl.classList.add('visually-hidden');
-    });
-}
+// function setOutput() {
+//   boxCatInfoEl.innerHTML = '';
+//   loaderEl.classList.remove('visually-hidden');
+//   return fetchCatByBreed(selectEl.value)
+//     .then(data => {
+//       boxCatInfoEl.innerHTML = createCatInfo(data);
+//       loaderEl.classList.add('visually-hidden');
+//     })
+//     .catch(err => {
+//       errorEl.classList.remove('is-hidden');
+//       loaderEl.classList.add('visually-hidden');
+//     });
+// }
 
-function createCatInfo(arr) {
-  return arr
-    .map(
-      ({ breeds, url }) => `<img src=${url} alt=${breeds[0].name} width=600>
-<div class="text-box"><h2>${breeds[0].name}</h2>
-<p>${breeds[0].description}</p>
-<p><span class="temp-style">Temperament: </span>${breeds[0].temperament}</p>
-</div>`
-    )
-    .join('');
-}
+// function createCatInfo(arr) {
+//   return arr
+//     .map(
+//       ({ breeds, url }) => `<img src=${url} alt=${breeds[0].name} width=600>
+// <div class="text-box"><h2>${breeds[0].name}</h2>
+// <p>${breeds[0].description}</p>
+// <p><span class="temp-style">Temperament: </span>${breeds[0].temperament}</p>
+// </div>`
+//     )
+//     .join('');
+// }
